c6cf2f223aa383e2de10c3afcd8a8d1fcde5ab03
thomasfoltz <thomasfoltz42@gmail.com>
2024-02-21 13:04:09 -0500

    initial model implementation

---
+++ model.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Convolution(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+        self.relu = nn.ReLU()
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.conv = Convolution(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+        return x, p
+
+class Decoder(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2)
+        self.conv = Convolution(out_c + out_c, out_c)
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = F.interpolate(x, size=skip.size()[2:])
+        x = torch.cat([x, skip], dim=1)
+        x = self.conv(x)
+        return x
+
+class UNET(nn.Module):
+    def __init__(self, input_channels=1, cond_channels=10):
+        super().__init__()
+        self.e1 = Encoder(input_channels + cond_channels, 64)
+        self.e2 = Encoder(64, 128)
+        self.e3 = Encoder(128, 256)
+        self.e4 = Encoder(256, 512)
+        self.b = Convolution(512, 1024)
+        self.d1 = Decoder(1024, 512)
+        self.d2 = Decoder(512, 256)
+        self.d3 = Decoder(256, 128)
+        self.d4 = Decoder(128, 64)
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+
+    def forward(self, inputs, cond):
+        cond_expanded = cond.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, inputs.size(2), inputs.size(3))
+        inputs_cond = torch.cat([inputs, cond_expanded], dim=1)
+        s1, p1 = self.e1(inputs_cond)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        b = self.b(p4)
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        outputs = self.outputs(d4)
+        return outputs
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Flatten(),
+            nn.Linear(256 * 4 * 4, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img, cond):
+        cond_expanded = cond.unsqueeze(2).unsqueeze(3).expand(-1, -1, img.size(2), img.size(3))
+        img_cond = torch.cat([img, cond_expanded], dim=1)
+        return self.model(img_cond)
