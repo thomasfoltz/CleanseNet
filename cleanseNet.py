280866f4c416f1530c734eeeb8afccd808614e5d
thomasfoltz <thomasfoltz42@gmail.com>
2024-04-16 13:39:15 -0400

    GAN architecture and training

--- cleanseNet.py
+++ cleanseNet.py
@@ -1,7 +1,9 @@
 import torch
 import torchvision
+import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from models import Generator, Discriminator
 from pertubation import fgsm_attack, PurturbedDataset
 
 if __name__ == "__main__":
@@ -12,6 +14,63 @@ if __name__ == "__main__":
     testset = datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
 
     purturbed_trainset = PurturbedDataset(trainset, fgsm_attack, device)
+    dataloader = torch.utils.data.DataLoader(purturbed_trainset, batch_size=32, shuffle=True)
 
-    torchvision.utils.save_image(trainset[1][0], 'image_original.png')
-    torchvision.utils.save_image(purturbed_trainset[1][0], 'image_purturbed.png')
+    generator = Generator().to(device)
+    discriminator = Discriminator().to(device)
+
+    criterion = nn.BCELoss()
+    optimizer_G = torch.optim.Adam(generator.parameters())
+    optimizer_D = torch.optim.Adam(discriminator.parameters())
+
+    epochs = 10
+    for epoch in range(epochs):
+        for i, (purturbed_imgs, real_imgs, _) in enumerate(dataloader):
+            print(f"Epoch: {epoch}, Batch: {i}")
+            real_images = real_imgs.to(device)
+            purturbed_images = purturbed_imgs.to(device)
+            b_size = real_images.size(0)
+
+            # Create real and fake labels
+            real_labels = torch.ones(b_size, 1).to(device)
+            fake_labels = torch.zeros(b_size, 1).to(device)
+
+            # Train the Discriminator
+            optimizer_D.zero_grad()
+            # print(real_images.size())
+            outputs = discriminator(real_images)
+            # print(outputs.size(), real_labels.size())
+            real_loss = criterion(outputs, real_labels)
+            real_score = outputs
+
+            # Generate fake images
+            fake_images = generator(purturbed_images)
+            # print(fake_images.size())
+            outputs = discriminator(fake_images.detach())
+            # print(outputs.size(), fake_labels.size())
+            fake_loss = criterion(outputs, fake_labels)
+            fake_score = outputs
+
+            # Backprop and optimize
+            d_loss = real_loss + fake_loss
+            d_loss.backward()
+            optimizer_D.step()
+
+            # Train the Generator
+            optimizer_G.zero_grad()
+            fake_images = generator(purturbed_images)
+            outputs = discriminator(fake_images)
+
+            # Measure generator's ability to fool the discriminator
+            g_loss = criterion(outputs, real_labels)
+
+            # Backprop and optimize
+            g_loss.backward()
+            optimizer_G.step()
+
+
+    torch.save(generator.state_dict(), 'generator_model.pth')
+    torch.save(discriminator.state_dict(), 'discriminator_model.pth')
+
+    # torchvision.utils.save_image(trainset[1][0], 'image_original.png')
+    # torchvision.utils.save_image(purturbed_trainset[1][0], 'image_purturbed.png')
---
+++ models.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(3, 256, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 3, 1, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        output = output.view(output.size(0), -1)
+        return output
